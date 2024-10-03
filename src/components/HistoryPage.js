4aaf12dfa836292df97bd877d5756aeb34447597
Markus Haugsdal <markus.haugsdal@sonat.no>
2024-10-03 14:20:53 +0200

    suppress warning

--- src/components/HistoryPage.js
+++ src/components/HistoryPage.js
@@ -3,6 +3,7 @@ import EarlyYears from './EarlyYears';
 
 const HistoryPage = () => (
   <div className="container">
+      {/* eslint-disable-next-line jsx-a11y/heading-has-content */}
     <h1></h1>
     <p></p>
     <EarlyYears />
