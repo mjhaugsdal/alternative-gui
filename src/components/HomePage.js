e89001858930ba6f24654a50c3e3295567232436
Markus Haugsdal <markus.haugsdal@sonat.no>
2025-01-08 16:16:36 +0100

    push

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage = () => (
   <div className="container">
-    <h1>Glimts Alternative</h1>
+    <h1>Glimts Andre</h1>
     {/*<p>Your one-stop hub for all things related to our beloved football club.</p>*/}
   </div>
 );
